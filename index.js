8805be1f045eb044a32ad64de36d093629e10c34
James Sinkler <jamessinkler713@gmail.com>
2021-02-04 11:03:56 -0800

    server up and running

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // gives us access to variables in our .env
 require('dotenv').config()
-
 // see variables using process.env
-console.log(process.env)
-console.log(process.env.API_KEY)
+// console.log(process.env)
+// console.log(process.env.API_KEY)
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+
+
+// MiddleWare
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+// Routes
+app.get('/', (req, res)=> {
+    res.send('hello')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Listening on ${PORT}`))
+
